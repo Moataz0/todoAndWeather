e147163b4e63f520189d474b47fafe11de3690b6
Moataz0 <zo.o@hotmail.com>
2023-12-18 05:21:27 +0200

    Remove link

--- src/components/DetailedWeatherPage.js
+++ src/components/DetailedWeatherPage.js
@@ -46,7 +46,6 @@ const DetailedWeatherPage = () => {
   useEffect(() => {
     const fetchWeatherData = async (latitude, longitude) => {
       const apiKey = "YOUR_OPENWEATHERMAP_API_KEY";
-      // const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&cnt=5`;
       const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=minutely&appid=${apiKey}`;
 
       try {
